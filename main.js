462f7cc0f0e0a08b8c64205328eb32d2f194023c
Skyler Espinoza <Skyler.Espinoza@sckans.edu>
2021-04-21 12:37:00 -0500

    FP

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from './pkg/three.module.js'; 
+import { Vector2 } from './Three/pkg/three.module.js';
 
 window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
 
@@ -7,6 +8,7 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
     render.setClearColor ('#0ff');
     render.shadowMap.enabled = true;
     const scene = new THREE.Scene();
+    scene.fog = new THREE>FogExp2(0xFFFFFF, 0.1);
     const camera = new THREE.PerspectiveCamera(75, render.donElement.clientWidth / render.donElement.clientHeight, 0.1, 1000); 
     camera.position.z = 5;
     const resize = () => {
@@ -47,8 +49,24 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
         roughness: 0.75,
     });
     const cube = new THREE.Mesh(cube_geometry, cube_material);
+    cube.name = 'hittable';
     cube.castShadow = true;
     scene.add(cube);
+
+    // INPUT MONITORING
+    const input = {w: false, a: false, s: false, d: false, ArrowLeft: false, ArrowRight: false, f: false}
+    window.addEventListener('keydown', keydown => {
+        if(input.hasOwnProperty(keydown.key)){
+            input[keydown.key] = true;
+        }
+
+    });
+    window.addEventListener('keyup', keyup => {
+        if(input.hasOwnProperty(keyup.key)){
+            input[keydown.key] = false;
+        }
+
+    });
 
 
     // ANIMATION LOOP
@@ -57,10 +75,26 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
         // REQUEST NEXT FRAME
         window.requestAnimationFrame(animate);
 
-        // ROTATE CUBE
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        // CAMERA
+        const speed = 0.1;
+        camera.rotation.y += speed / Math.PI * (input.ArrowLeft - input.ArrowRight);
+        const velocity = new THREE.Vector3(speed * (input.a - input.d), 0, speed * (input.s - input.w));
+        camera.position.x += velocity.x * -Math.cos(camera.rotation.y) + velocity.z * Math.sin(camera.rotation.y);
+        camera.position.z += velocity.x * Math.sin(camera.rotation.y) + velocity.z * Math.cos(camera.rotation.y);
 
+        // RAYCASTING
+        cube.material.color.set(0x0000FF);
+        if(input.f){
+            const player_raycast = new THREE.Raycaster();
+        player_raycast.camera.setFromCamera(new THREE>Vector2(0, 0), camera);
+        const intersects player_raycast.intersectObjects(scene.children);
+        intersects?.forEach(hit_object => {
+            if(hit_object.object.name === 'hittable'){
+                hit_object.object.material.color.set(0xFF0000);
+            }
+        });
+        
+        }
 
         // RENDER
         render.render(scene, camera);
